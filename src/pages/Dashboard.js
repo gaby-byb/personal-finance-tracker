0c082cd0a71f427d5772fa0b27a19a20df39b4db
gaby-byb <gabyatl@outlook.com>
2025-02-09 19:00:13 -0500

    Set up structure and Created Dashboard page

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h2>Dashboard Page</h2>
+            <p>Welcome to Gaby's finance tracker!</p>
+         </div>
+    );
+};
+
+export default Dashboard;
